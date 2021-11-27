35b228a5696364538315261edf18c29ff188d31f
TienNguyen93 <nguyentien9933@gmail.com>
2021-11-27 17:14:32 -0500

    add login screen

--- App.js
+++ App.js
@@ -3,23 +3,29 @@ import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./Screens/Home";
-import Profile from "./Screens/Profile";
+import Profile from "./Screens/Profile"
+import Login from "./Screens/Login";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Home"
           component={Home}
           options={{ title: "Home" }}
         />
+        {/*<Stack.Screen*/}
+        {/*  name="Profile"*/}
+        {/*  component={Profile}*/}
+        {/*  options={{ title: "Profile" }}*/}
+        {/*/>*/}
         <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{ title: "Profile" }}
+            name="Login"
+            component={Login}
+            options={{ title: "Login", headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
