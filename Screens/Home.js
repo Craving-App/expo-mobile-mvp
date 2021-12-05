948260016a6b835e104c39203df608425b163450
lishannon <lishannon490@gmail.com>
2021-12-04 23:08:34 -0500

    Created the success screen

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet,TouchableOpacity,Text } from "react-native";
 
 export default function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Page</Text>
-      <Text onPress={() => navigation.navigate("Profile")}>
-        Click me to view profile
-      </Text>
+      <Text style={{ textAlign: "center",  fontSize: 16, fontWeight: 'bold'}}>Success!</Text>
+      <Text style={{ textAlign: "center", padding: 40}}>Onboarding all completed, get mingling.</Text>
+      <TouchableOpacity onPress={() => alert('You clicked the success button!')} style={styles.button}>
+        <Text style={styles.buttonText} >Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "orange",
+    padding: 15,
+    borderRadius: 20,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold'
+  }, 
+});
