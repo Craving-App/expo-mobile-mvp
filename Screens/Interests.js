c383516f015c9b5e8651bef33d2ee76f09d3d13d
ragib <ragib>
2022-01-15 20:03:37 -0500

    Add prop to change between styling

--- Screens/Interests.js
+++ Screens/Interests.js
@@ -5,13 +5,13 @@ import Pills from "../components/Pills";
 const Interests = ({ navigation }) => {
     return (
         <View style={styles.center}>
-            <Pills name="Hiking" />
-            <Pills name="Running" />
-            <Pills name="Gaming" />
-            <Pills name="Cooking" />
-            <Pills name="Cycling" />
-            <Pills name="Netflix" />
-            <Pills name="Fashion" />
+            <Pills name="Hiking" common="true" />
+            <Pills name="Running" common="true" />
+            <Pills name="Gaming" common="true" />
+            <Pills name="Cooking" common="false" />
+            <Pills name="Cycling" common="false" />
+            <Pills name="Netflix" common="false" />
+            <Pills name="Fashion" common="false" />
         </View>
     );
 };
--- components/Pills.js
+++ components/Pills.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { render } from "react-dom";
 import { Text, View, StyleSheet } from "react-native";
 
 const Pills = (props) => {
+    let relatedStyles;
+    if (props.common === "true") {
+        relatedStyles = styles.common;
+    } else {
+        relatedStyles = styles.uncommon;
+    }
     return (
-        <View style={styles.view}>
+        <View style={[styles.view, relatedStyles]}>
             <Text style={styles.text}>{props.name}</Text>
         </View>
     );
@@ -15,16 +22,28 @@ const styles = StyleSheet.create({
         height: 30,
         width: 110,
         padding: 5,
-        backgroundColor: "#e36d71",
         borderRadius: 19,
         margin: 5,
+        borderColor: "darkgreen",
+        borderWidth: 1,
     },
     text: {
-        color: "#ffffff",
+        color: "#000",
         fontWeight: "bold",
         textTransform: "uppercase",
         fontSize: 16,
         textAlign: "center",
+    },
+    common: {
+        backgroundColor: "#fa6d71",
+        shadowColor: "#000",
+        shadowOffset: { width: 1, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5,
+    },
+    uncommon: {
+        backgroundColor: "transparent",
     },
 });
 
